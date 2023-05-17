55a28a926978a2e6eeac9727e939496dee407600
Tim McGuffin <17074002+NotMedic@users.noreply.github.com>
2023-05-17 09:13:52 -0500

    Initial Commit

---
+++ SharpCOMDumpster/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace SharpCOMDumpster
+{
+	public static class ComSvcs
+	{
+		[DllImport("comsvcs.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+		public static extern bool MiniDumpW(
+			IntPtr Dummy1,
+			IntPtr Dummy2,
+			string lpArg
+		);
+	}
+	public static class Kernel32
+	{
+		[DllImport("kernel32.dll")]
+		public static extern uint GetLastError();
+	}
+}
